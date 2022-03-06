b1995fcaf2e3294f96f89df89adc40b12cbc8c77
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-03-05 21:13:48 -0500

    fixed text alignment

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div class="bg-img"></div>
 <div class="content">
     <div class="row">
-        <div class="col-lg-8 col-sm-12 text-sm-center text-md-center mb-2">
+        <div class="col-lg-8 col-sm-12 text-sm-center text-md-left mb-2">
             <h1>Welcome to <span class="brand-font">NAVBAR</span></h1>
             <p>Get ready for the newest bar on the block. Be the first to learn about our <strong>GRAND OPENING</strong> by signing up to join our newsletter, The NAVBAR Journal. You won't want to miss out!</p>
             <h2 class="neon-sign">COMING SOON</h2>
