c7c6ed6779eba8a892d714ed9984101be8175edf
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-22 16:32:54 -0400

    Create database.php

---
+++ functions/database.php
@@ -0,0 +1,65 @@
+<?php
+    // global connection
+    $connection = null;
+
+    // connect to database
+    function database_connect() {
+        global $connection;
+
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "NAVBAR";
+
+        if($connection == null) {
+            $connection = mysqli_connect($server, $username, $password, $database);
+        }
+    }
+
+    function database_addUser($username, $password) {
+        // Use the global connection
+        global $connection;
+
+        if($connection != null) {
+            // Overwrite the existing password value as a hash
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            // Insert username and hashed password
+            mysqli_query($connection, "INSERT INTO users (username, password) VALUES ('{$username}', '{$password}');");
+        }
+    }
+
+    function database_verifyUser($username, $password) {
+        // Use the global connection
+        global $connection;
+
+        // Create a default value
+        $status = false;
+
+        if($connection != null) {
+            // Use WHERE expressions to look for username
+            $results = mysqli_query($connection, "SELECT password FROM users WHERE username = '{$username}';");
+            
+            // mysqli_fetch_assoc() returns either null or row data
+            $row = mysqli_fetch_assoc($results);
+            
+            // If $row is not null, it found row data.
+            if($row != null) {
+                // Verify password against saved hash
+                if(password_verify($password, $row["password"])) {
+                    $status = true;
+                }
+            }
+        }
+
+        return $status;
+    }
+
+    // close database
+    function database_close() {
+        global $connection;
+
+        if($connection != null) {
+            mysqli_close($connection);
+        }
+    }
+?>
