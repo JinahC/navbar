e6e3fc77a2aeaaf5bf0a0de953fcc9fa91b04f0d
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-22 02:31:43 -0400

    set up the layout for content

--- index.php
+++ index.php
@@ -1,31 +1,40 @@
 <?php include "templates/header.php" ?>
 
 <!-- CONTENT -->
-<div class="content-container">
-<div class="bg-img"></div>
-<div class="content">
-    <div class="row">
-        <div class="col-lg-8 col-sm-12 text-sm-center text-md-left mb-2">
-            <h1>Welcome to <span class="brand-font">NAVBAR</span></h1>
-            <p>Get ready for the newest bar on the block. Be the first to learn about our <strong>GRAND OPENING</strong> by signing up to join our newsletter, The NAVBAR Journal. You won't want to miss out!</p>
-            <h2 class="neon-sign">COMING SOON</h2>
-        </div>
-        <div class="col-lg-4 col-sm-12">
-            <div class="flex justify-content-center my-2">
-                <form action="signup.php" method="POST">
-                    <h3 class="brand-font form-header">The NAVBAR Journal</h3>
-                    <label for="first_name">First Name</label><br>
-                    <input type="text" id="first_name" name="user_fname" required><br><br>
-                    <label for="last_name">Last Name</label><br>
-                    <input type="text" id="last_name" name="user_lname" required><br><br>
-                    <label for="email">Email</label><br>
-                    <input type="text" id="email" name="user_email" required><br><br>
-                    <input type="submit" name="submit" value="Sign Up!">
-                </form>
-            </div>
-        </div>
+<div class=" billboard billboard-full">
+    <div class="billboard-text">
+        <h1 class="font-display">welcome to the</h1>
+        <h1 class="font-brand">navbar</h1>
     </div>
 </div>
+<div class="container">
+    <hr>
+    <p class="font-display">explore the</p>
+    <p class="font-display">unknown</p>
+    <hr>
+    <p>We’re the newest and bluest bar on the scene. Discover the mysteries of a hidden gem on the eastern seaboard. You can enjoy a night under the stars on the roof or party until the break of dawn below deck. Either way, you’re bound to make waves at the NAVBAR.</p>
+    <a class="btn" href="about.php">learn more</a>
 </div>
+<div class="banner">
+    <img class="banner-img" style="width: 100%;" src="img/bar-bg.jpg">
+</div>
+<div class="container">
+    
+    <div id="newsletter">
+        <h3>subscribe to our</h3>
+        <h3>the navbar journal</h3>
+        <form action="" method="POST">
+            <label for=""></label><br>
+            <input type="text" name="" id=""><br><br>
+            <label for=""></label><br>
+            <input type="text" name="" id=""><br><br>
+            <label for=""></label><br>
+            <input type="text" name="" id=""><br><br>
+            <input type="submit" name="submit" value="subscribe">
+        </form>
+    </div>
+</div>
+
+
 
 <?php include "templates/footer.php" ?>
