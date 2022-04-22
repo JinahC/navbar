3fe587b214cb90dc3238564f2a093f16a0801a62
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-22 03:14:07 -0400

    added content

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,5 @@
-<footer class="py-3">
-    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-    viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+<footer class="pt-3">
+    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
         <defs>
         <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
         </defs>
@@ -11,8 +10,9 @@
         <use xlink:href="#gentle-wave" x="48" y="7" fill="rgb(63, 120, 241)" />
         </g>
     </svg>
-    <div style="background-color: rgb(63, 120, 241);">
-        <h1>navbar</h1>
+    <div class="footer-content pt-4 pb-2">
+        <h1 class="font-brand">navbar</h1>
+        <p>1441 Sailor St, Ft. Lauderdale, FL 33311</p>
     </div>
 </footer>
 
