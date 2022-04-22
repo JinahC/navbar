4a81ff3addb5ff67535ad2d15a627f2243a82139
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-22 02:32:04 -0400

    made it wavy :D

--- templates/footer.php
+++ templates/footer.php
@@ -1,2 +1,20 @@
+<footer class="py-3">
+    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+        <defs>
+        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g class="parallax">
+        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(63, 120, 241,0.7" />
+        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(63, 120, 241,0.5)" />
+        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(63, 120, 241,0.3)" />
+        <use xlink:href="#gentle-wave" x="48" y="7" fill="rgb(63, 120, 241)" />
+        </g>
+    </svg>
+    <div style="background-color: rgb(63, 120, 241);">
+        <h1>navbar</h1>
+    </div>
+</footer>
+
 </body>
 </hmtl>
