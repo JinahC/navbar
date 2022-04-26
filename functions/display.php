869601d8f93b59f2195974d624b2b866394a89dd
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-26 03:07:08 -0400

    created display events function

--- functions/display.php
+++ functions/display.php
@@ -1,16 +1,34 @@
 <?php
+include("database.php");
 
-function saveNewSignup($fname, $lname, $email) {
-    $file = fopen("signups.txt", "a");
-    fwrite($file, $fname . " " . $lname . " " . $email . "\n");
-
-    fclose($file);
-}
-
-function displayMenu() {
+// output menu items to the screen
+function display_menu() {
     return;
 }
 
 function displayBoxes() {
     return;
+}
+
+// output events to the screen
+function display_events() {
+    database_connect();
+
+    $results = database_grabEvents();
+
+    echo "<div class='row'>";
+
+    while ($events=mysqli_fetch_assoc($results)) {
+        echo "
+        <div class='col-lg-4 col-md-6 col-sm-12 my-3 text-center'>
+            <img src='{$events['event_img']}' class='event-img'>
+            <h3 class='font-brand'>{$events['event_name']}</h3>
+            <p class='font-display'>{$events['event_day']} @ {$events['event_time']}</p>
+        </div>
+        ";
+    }
+
+    echo "</div>";
+
+    database_close();
 }
