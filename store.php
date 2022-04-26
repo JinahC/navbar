6df8c6455581f788cd08ad964cdcbc252774aaad
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-26 03:05:01 -0400

    Create store.php

---
+++ store.php
@@ -0,0 +1,13 @@
+<?php 
+include "templates/header.php"; 
+?>
+
+<!-- billboard -->
+<div class=" billboard billboard-half" id="billboard-store">
+    <div class="billboard-text">
+        <h1 class="font-brand">store</h1>
+    </div>
+</div>
+
+
+<?php include "templates/footer.php";
