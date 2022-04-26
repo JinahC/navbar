66240b0bf064ba289317e2e205babd2b79c65f6f
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-26 16:15:36 -0400

    filled out function to display menu

--- functions/display.php
+++ functions/display.php
@@ -3,7 +3,47 @@ include("database.php");
 
 // output menu items to the screen
 function display_menu() {
-    return;
+    database_connect();
+
+    echo "
+    <h2 class='font-brand'>menu</h3>
+    <div class='row'>
+        <div class='col-lg-6'>
+            <h3 class='font-display'>food</h3>
+    ";
+
+    $results = database_grabMenuItems("food");
+
+    while ($food_items = mysqli_fetch_assoc($results)) {
+        echo "
+        <div class='menu-item'>
+            <p class='menu-item-name'>{$food_items['item_name']}</p>
+            <p class='menu-item-ingredients'>{$food_items['ingredients']}</p>
+            <p class='menu-item-price'>{$food_items['price']}</p>
+        </div>
+        ";
+    }
+
+    echo "
+    </div>
+    <div class='col-lg-6'>
+        <h3 class='font-display'>drinks</h3>
+    ";
+
+    $results = database_grabMenuItems("drinks");
+
+    while ($food_items = mysqli_fetch_assoc($results)) {
+        echo "
+        <div class='menu-item'>
+            <p class='menu-item-name'>{$food_items['item_name']}</p>
+            <p class='menu-item-ingredients'>{$food_items['ingredients']}</p>
+            <p class='menu-item-price'>{$food_items['price']}</p>
+        </div>
+        ";
+    }
+
+    echo "</div></div>";
+    database_close();
 }
 
 function displayBoxes() {
@@ -18,7 +58,7 @@ function display_events() {
 
     echo "<div class='row'>";
 
-    while ($events=mysqli_fetch_assoc($results)) {
+    while ($events = mysqli_fetch_assoc($results)) {
         echo "
         <div class='col-lg-4 col-md-6 col-sm-12 my-3 text-center'>
             <img src='{$events['event_img']}' class='event-img'>
