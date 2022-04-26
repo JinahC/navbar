3a40ef4d5e6278c17f17f23afd45bb05cff1ace5
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-26 03:05:04 -0400

    Create reservations.php

---
+++ reservations.php
@@ -0,0 +1,55 @@
+<?php 
+include "templates/header.php"; 
+?>
+
+<!-- billboard -->
+<div class=" billboard billboard-half" id="billboard-reservations">
+    <div class="billboard-text">
+        <h1 class="font-display">hop on</h1>
+        <h1 class="font-brand">board</h1>
+    </div>
+</div>
+
+<!-- book a table -->
+<div class="container text-center">
+    <h2 class="font-brand">book a table</h2>
+    <hr>
+    <p>Need to secure a table for a fun night out at sea? We offer table reservations for parties of 2 to 8 guests. Book a table below!</p>
+    <a class="btn d-block my-3" href="#">find a table</a>
+</div>
+<!-- plan a party -->
+<div class="bg-ocean">
+    <div class="container">
+        <h2 class="font-brand text-center">plan a party</h2>
+        <hr>
+        <div class="row text-sm-center">
+            <div class="col-lg-8 col-sm-12 text-lg-left">
+                <h3 class="font-brand">keep it grounded</h3>
+                <p>Keep the party below deck and show’em how it’s done. From the moment your guests walk in, they will be transported to a night on the original North Star. Portraits and ship parts are posted along the walls for the guests to admire. Your party of 40 to 100 guests can enjoy live entertainment, an open bar, and food aplenty. Get ready to be the host with the most.</p>
+            </div>
+            <div class="col-lg-4 col-sm-12">
+                <img class="event-img" src="img/reservations-grounded.jpg">
+            </div>
+        </div>
+        <div class="row text-sm-center">
+            <div class="col-lg-4 col-sm-12">
+                <img class="event-img" src="img/reservations-rooftop.jpg">
+            </div>
+            <div class="col-lg-8 col-sm-12 text-lg-right">
+                <h3 class="font-brand">under the stars</h3>
+                <p>Ain’t no party like a rooftop party, cause a rooftop party don’t stop. The rooftop is decked out with cabanas, a bar paired with a private bartender, and a dance floor, and it comes with a great view of the beautiful and sparkling sea. Dance the night away with a DJ and a dance floor, or enjoy a night of fine dining. Perfect for mingling events for up to 60 guests.</p>
+            </div>
+        </div>
+        <div class="row text-sm-center">
+            <div class="col-lg-8 col-sm-12 text-lg-left">
+                <h3 class="font-brand">with the works</h3>
+                <p>Rock the boat with access to the entire venue: ground level and the rooftop! To make the most of the venue, it is recommended that you have a party of at least 70 people but no more than 150. You can set up the bar how you want. You can keep the party downstairs and have a beautiful collage of backdrops on the roof for pictures. Whatever you want, we got it!</p>
+            </div>
+            <div class="col-lg-4 col-sm-12">
+                <img class="event-img" src="img/reservations-the-works.jpg">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "templates/footer.php";
