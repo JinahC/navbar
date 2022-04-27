77e4382eb18b7064c87831cf2f658baca52e3d68
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-27 02:49:43 -0400

    Create mobile-nav.php

---
+++ templates/mobile-nav.php
@@ -0,0 +1,38 @@
+<div id="mobile-nav">
+    <div class="d-flex justify-content-end" id="x-div">
+        <a href="javascript:void(0);" id="x">
+            <span class="iconify" data-icon="bi:x" style="color: white; font-size: 100px;"></span>
+        </a>
+    </div>
+    <div class="">
+        <ul class="nav">
+            <li class="nav-item pb-1">
+                <a class="nav-link active" href="index.php">about</a>
+            </li>
+            <li class="nav-item pb-1">
+                <a class="nav-link" href="#">menu</a>
+            </li>
+            <li class="nav-item pb-1">
+                <a class="nav-link" href="events.php">events</a>
+            </li>
+            <li class="nav-item pb-1">
+                <a class="nav-link" href="reservations.php">reservations</a>
+            </li>
+            <li class="nav-item pb-1">
+                <a class="nav-link" href="#">newsletter</a>
+            </li>
+            <li class="nav-item pb-1">
+                <a class="nav-link" href="store.php">store</a>
+            </li>
+        </ul>
+        <a class="btn d-block my-1" href="mailto:info@navbar.com">contact</a>
+        <div class="row justify-content-center mt-4">
+            <span class="iconify mx-1" data-icon="ant-design:twitter-outlined" style="color: white; font-size: 49px;"></span>            
+            <span class="iconify mx-1" data-icon="ant-design:instagram-filled" style="color: white; font-size: 49px;"></span>
+            <span class="iconify mx-1" data-icon="ant-design:phone-filled" style="color: white; font-size: 49px;"></span>
+        </div>
+    </div>
+    <div class="bg-navy home-bar py-4">
+        <a class="font-brand home-link" href="index.php">NAVBAR</a>
+    </div>
+</div>
