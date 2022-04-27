970d5b8db2c7d72a309f56a0a3f80d7e5263f24c
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-27 02:17:59 -0400

    including mobile nav + js file

--- templates/header.php
+++ templates/header.php
@@ -17,8 +17,12 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+    <script src="js/script.js"></script>
 </head>
 <body>
+    <!-- MOBILE NAV -->
+    <?php include "mobile-nav.php" ?>
+
     <!-- HEADER -->
     <header class="row align-items-center p-3">
         <div class="col-lg-4 justify-content-start">
