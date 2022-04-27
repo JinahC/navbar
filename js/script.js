8c49507f86314275df4b83811323c5a5c37e7e97
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-27 02:18:53 -0400

    added functions for mobile nav
    - close nav
    - show nav

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// displays the mobile navigation when you click on the hamburger menu
+function showMobileNav() {
+    var navDiv = document.getElementById("mobileNav");
+    navDiv.style.display = "block";
+    document.body.style.overflow = "hidden";
+}
+
+// closes the mobile navigation when you click on the "x" or one of the links
+function closeMobileNav() {
+    var navDiv = document.getElementById("mobileNav");
+    navDiv.style.display = "none";
+    document.body.style.overflow = "initial";
+}
+
+// initialization
+function init() {
+    var bars = document.getElementById("bars");
+    var x = document.getElementById("x");
+
+    bars.addEventListener("click", showMobileNav, false);
+    x.addEventListener("click", closeMobileNav, false);
+}
+
+// ...
+window.addEventListener("load", init, false);
