e83bf846c02b5ebdcc4747b62486fba1c5b83044
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-27 02:50:36 -0400

    corrected mobile nav id

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // displays the mobile navigation when you click on the hamburger menu
 function showMobileNav() {
-    var navDiv = document.getElementById("mobileNav");
+    var navDiv = document.getElementById("mobile-nav");
     navDiv.style.display = "block";
     document.body.style.overflow = "hidden";
 }
 
 // closes the mobile navigation when you click on the "x" or one of the links
 function closeMobileNav() {
-    var navDiv = document.getElementById("mobileNav");
+    var navDiv = document.getElementById("mobile-nav");
     navDiv.style.display = "none";
     document.body.style.overflow = "initial";
 }
 
 // initialization
 function init() {
-    var bars = document.getElementById("bars");
+    // var bars = document.getElementById("bars");
     var x = document.getElementById("x");
 
-    bars.addEventListener("click", showMobileNav, false);
+    // bars.addEventListener("click", showMobileNav, false);
     x.addEventListener("click", closeMobileNav, false);
 }
 
