62ce2d8fccb67b516887d26d8acef191765bd309
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-27 03:52:28 -0400

    added responsiveness for headers

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 // displays the mobile navigation when you click on the hamburger menu
 function showMobileNav() {
     var navDiv = document.getElementById("mobile-nav");
+    var headers = document.getElementsByTagName("header");
+
     navDiv.style.display = "block";
+    for (var i = 0; i < headers.length; i++)
+        headers[i].style.display = "none";
     document.body.style.overflow = "hidden";
 }
 
 // closes the mobile navigation when you click on the "x" or one of the links
 function closeMobileNav() {
     var navDiv = document.getElementById("mobile-nav");
+    var headers = document.getElementsByTagName("header");
+
     navDiv.style.display = "none";
+    for (var i = 0; i < headers.length; i++)
+        headers[i].style.display = "block";
     document.body.style.overflow = "initial";
 }
 
