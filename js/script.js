1ef4465720724112b2c3ecd1a5279f39f787a4f2
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-28 22:30:52 -0400

    added function to validate input
    - clear an element
    - validate user input

--- js/script.js
+++ js/script.js
@@ -20,6 +20,57 @@ function closeMobileNav() {
     document.body.style.overflow = "initial";
 }
 
+// clears the content of the given element 
+function clearElement(el) {
+    while(el.hasChildNodes()) {
+        el.removeChild(el.lastChild);
+    }
+}
+
+// validates the input in the newsletter form
+function validateInput(e) {
+    // e.preventDefault();
+    var input = e.target;
+    var inputID = input.getAttribute("id");
+    console.log("the input is " + input + " and the id is " + inputID);
+
+    var spans = document.getElementsByClassName("input-warning");
+    var warnings = ["Enter only letters", "Enter only letters", "Enter a vaild email"];
+    var myRe = [/^[A-Za-z]+$/, /^[A-Za-z]+$/, /[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]+(\.[A-Za-z])*/];
+
+    console.log("second check for id: " + inputID);
+    // checks first name
+    if (inputID == "fname") {
+        console.log("im checking first name");
+        clearElement(spans[0]);
+
+        if (!input.value.match(myRe[0])) {
+            var warningText = document.createTextNode(warnings[0]);
+            spans[0].appendChild(warningText);
+        }
+    }
+    // checks last name
+    else if (inputID == "lname") {
+        console.log("im checking last name");
+        clearElement(spans[1]);
+
+        if (!input.value.match(myRe[1])) {
+            var warningText = document.createTextNode(warnings[1]);
+            spans[1].appendChild(warningText);
+        }
+    }
+    // checks email
+    else if (inputID == "email") {
+        console.log("im checking email");
+        clearElement(spans[2]);
+
+        if (!input.value.match(myRe[2])) {
+            var warningText = document.createTextNode(warnings[2]);
+            spans[2].appendChild(warningText);
+        }
+    }
+}
+
 // initialization
 function init() {
     var bars = document.getElementById("bars");
@@ -27,6 +78,17 @@ function init() {
 
     bars.addEventListener("click", showMobileNav, false);
     x.addEventListener("click", closeMobileNav, false);
+
+
+    var first = document.getElementById("fname");
+    var last = document.getElementById("lname");
+    var email = document.getElementById("email");
+    var subscribe = document.getElementById("newsletter-submit");
+
+    first.addEventListener("blur", validateInput, false);
+    last.addEventListener("blur", validateInput, false);
+    email.addEventListener("blur", validateInput, false);
+    // subscribe.addEventListener("click", validateInput, false);
 }
 
 // ...
