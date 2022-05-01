931341ace3a6cbe17bcf6ba8d31f34840dd74aeb
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-04-30 22:25:06 -0400

    Create book-a-table.php

---
+++ popups/book-a-table.php
@@ -0,0 +1,9 @@
+<div class="popup container" id="bat-popup">
+    <div class="d-flex justify-content-end" id="x-div">
+        <a href="javascript:void(0);" id="x">
+            <span class="iconify" data-icon="bi:x" style="color: white; font-size: 100px;"></span>
+        </a>
+    </div>
+    <h1 class="font-brand">book a table</h1>
+    <p>can book for up to 8 people</p>
+</div> 
